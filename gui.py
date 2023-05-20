d68cd94b02d9f30fd3d3430b02851bd8a5153415
JiauZhang <1743960454@qq.com>
2023-05-20 23:53:45 +0800

    update gui

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ width, height, channel = 256, 256, 3
 noise = np.random.uniform(0, 1, size=(width, height, channel))
 
 dpg.create_context()
-dpg.create_viewport(title='DragGAN', width=800, height=600)
+dpg.create_viewport(title='DragGAN', width=800, height=650)
 
 with dpg.texture_registry(show=False):
     dpg.add_raw_texture(
@@ -13,11 +13,36 @@ with dpg.texture_registry(show=False):
         format=dpg.mvFormat_Float_rgb, tag="image"
     )
 
-with dpg.window(tag="Primary Window"):
-    dpg.add_image("image")
+width, height = 200, 200
+posx, posy = 0, 0
+with dpg.window(
+    label='Network & Latent', width=width, height=height, pos=(posx, posy),
+    no_move=True, no_close=True, no_collapse=True, no_resize=True,
+):
+    ...
+
+posy += height + 2
+with dpg.window(
+    label='Drag', width=width, height=height, pos=(posx, posy),
+    no_move=True, no_close=True, no_collapse=True, no_resize=True,
+):
+    ...
+
+posy += height + 2
+with dpg.window(
+    label='Capture', width=width, height=height, pos=(posx, posy),
+    no_move=True, no_close=True, no_collapse=True, no_resize=True,
+):
+    ...
+
+posx, posy = 2 + width, 0
+with dpg.window(
+    label='Image', pos=(posx, posy),
+    no_move=True, no_close=True, no_collapse=True, no_resize=True,
+):
+    dpg.add_image("image", show=True)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
